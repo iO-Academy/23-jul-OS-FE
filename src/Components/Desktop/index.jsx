17dd30c3037bf00aaec917fc185514c1675d7509
iO Academy <thejoshsilasbryan@gmail.com>
2023-10-16 14:47:26 +0100

    programme buttons added

--- src/Components/Desktop/index.jsx
+++ src/Components/Desktop/index.jsx
@@ -3,7 +3,7 @@ import WallpaperCircle from "../WallpaperCircle/index.jsx";
 const Desktop = () => {
     return (
         <div className={"min-h-screen bg-green-600 text-center text-color-black mt-0 text-3xl pt-10"}>
-            <WallpaperCircle />
+            {/*<WallpaperCircle />*/}
             <div className ="z-10 fixed top-10 left-0 right-0"><ProgramSelector /> </div>
         </div>
     )
--- src/Components/WallpaperCircle/index.jsx
+++ src/Components/WallpaperCircle/index.jsx
@@ -4,7 +4,7 @@ const WallpaperCircle = () => {
     return (
 
         <div className = "flex justify-center">
-            <container className="circle rounded-full bg-green-900 bg-opacity-45 mt-0 z-10 flex justify-center flex-wrap flex-col content-center">
+            <container className="circle rounded-full bg-green-900 bg-opacity-45 mt-0 flex justify-center flex-wrap flex-col content-center">
                 <div className ="text-center mt-5">Welcome</div>
                 <div className ="text-center mt-5">User</div>
                 <div className ="bg-red-800 text-center mt-5">User Icon</div>
