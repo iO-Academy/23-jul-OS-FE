a09220c632ad76ed570c0fecf3cd38ca24e6c85e
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-10-16 10:53:11 +0100

    Made NavBar

--- src/Components/Desktop/index.jsx
+++ src/Components/Desktop/index.jsx
@@ -1,7 +1,9 @@
+import NavBar from "../NavBar/index.jsx";
+
 const Desktop = () => {
     return (
         <div className={"min-h-screen bg-green-600 text-center text-color-black text-3xl pt-20"}>
-
+            <NavBar />
         </div>
     )
 }
---
+++ src/Components/NavBar/index.jsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+
+    return (
+        <div className={"bg-purple-200"}>
+            <span>Logout</span>
+            <span>Menu</span>
+            <span>Clock</span>
+        </div>
+    )
+}
+
+export default NavBar
