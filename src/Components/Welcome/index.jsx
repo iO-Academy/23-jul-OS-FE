9573249ee963ad7208d5fd05c911fdfdbcf34ab4
iO Academy <thejoshsilasbryan@gmail.com>
2023-10-17 11:36:52 +0100

    On click added

--- src/Components/Welcome/index.jsx
+++ src/Components/Welcome/index.jsx
@@ -1,7 +1,13 @@
 import './Welcome.css'
 import StandardButton from "../StandardButton/index.jsx";
+import {Link} from "react-router-dom";
+
+
 
 const Welcome = () => {
+    const handleClick = () => {
+    }
+
     return (
         <>
             <div className = "flex justify-center flex-wrap flex-col content-center mt-10">
@@ -9,7 +15,7 @@ const Welcome = () => {
                 <p>CaterPillar OS</p>
                 <container className="WelcomeCircle mt-10 rounded-full bg-green-900 bg-opacity-45 mt-0 z-10 flex justify-center flex-wrap flex-col content-center">
                     <div className="littleCircle rounded-full bg-emerald-700 mt-20 flex justify-center">
-                        <StandardButton text ={'Enter'}/>
+                        <Link className='flex justify-center' to={"userselect"}><StandardButton text ={'Enter'} onClick={handleClick} /></Link>
                     </div>
                 </container>
             </div>
