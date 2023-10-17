8c3285481f7d330606faadd86b78c12234fae48a
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-10-17 16:14:09 +0100

    LM changes

--- src/Components/ProgramContainer/index.jsx
+++ src/Components/ProgramContainer/index.jsx
@@ -4,7 +4,7 @@ import Draggable from "react-draggable";
 const ProgramContainer = ({toggleProgram, text}) => {
     return (
         <Draggable handle="header">
-            <div className="program resize overflow-auto ">
+            <div className="program resize overflow-auto">
                 <header className="header h-16">
                     <span className="text-2xl">Program</span>
                     <span className="close-btn absolute top-2 right-2" onClick={() => toggleProgram(false)}>Close</span>
