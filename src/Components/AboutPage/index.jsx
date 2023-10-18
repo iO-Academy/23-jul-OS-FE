8a9fe9050b1601a6fe4836c973d896eecda73b81
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-10-18 10:03:00 +0100

    Started About Program content

---
+++ src/Components/AboutPage/index.jsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+    return (
+        <>
+            <p>Welcome to Caterpillar OS. This is a browser-based operating system that the team has built during our final week of iO Academy. The OS was primarily built on the front-end using React-based JavaScript and styled using Tailwind CSS, however we have included some back-end features using PHP to perform tasks such as recognizing a logged-in user and extracting data from an API we have built using an SQL database.</p>
+        </>
+    )
+}
+
+export default AboutPage
