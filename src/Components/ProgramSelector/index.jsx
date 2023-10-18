76e808443688c72f2e22194450f9b2c8edc99507
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-10-18 16:35:14 +0100

    Containment set up, program now opens on correct button click

--- src/Components/ProgramSelector/index.jsx
+++ src/Components/ProgramSelector/index.jsx
@@ -6,22 +6,10 @@ import AboutPage from "../AboutPage/index.jsx";
 
 const ProgramSelector = ({username, icon, visible, openMenu, openProgram, activeProgram}) => {
 
+    const[aboutProgramOpen, setAboutProgramOpen] = useState(false)
+
     const handleAbout = () => {
-       return <ProgramContainer text="About the OS">
-                <AboutPage />
-       </ProgramContainer>
-    }
-
-    const handleTheme = () => {
-        return <ProgramContainer />
-    }
-
-    const handleGiphy = () => {
-        return <ProgramContainer />
-    }
-
-    const handleGame = () => {
-        return <ProgramContainer />
+       setAboutProgramOpen(true)
     }
 
     return (
@@ -36,15 +24,19 @@ const ProgramSelector = ({username, icon, visible, openMenu, openProgram, active
         </div>
         <div className={visible ? "animatedCircleLeft border-2 border-black absolute left-24 top-2 rounded-full bg-emerald-700 flex justify-center" : "littleCircleLeft border-2 border-black absolute left-24 top-2 rounded-full bg-emerald-700 flex justify-center"}>
             <StandardButton text={'About'} handleClick={handleAbout}/>
+                {aboutProgramOpen && <ProgramContainer text="About the OS" toggleProgram={setAboutProgramOpen}>
+                                        <AboutPage />
+                                    </ProgramContainer>}
         </div>
         <div className={visible ? "animatedCircleLeft border-2 border-black absolute left-24 bottom-2 rounded-full bg-emerald-700 flex justify-center" : "littleCircleLeft border-2 border-black absolute left-24 bottom-2 rounded-full bg-emerald-700 flex justify-center"}>
-            <StandardButton text={'Theme'} handleClick={handleTheme}/>
+            <StandardButton text={'Theme'} />
+            {/*We can now slot in our individual program components as children in here, see About section above*/}
         </div>
         <div className={visible ? "animatedCircleRight border-2 border-black absolute right-24 top-2 rounded-full bg-emerald-700 flex justify-center" : "littleCircleRight border-2 border-black absolute right-24 top-2 rounded-full bg-emerald-700 flex justify-center"}>
-            <StandardButton text={'Game'} handleClick={handleGame}/>
+            <StandardButton text={'Game'} />
         </div>
         <div className={visible ? "animatedCircleRight border-2 border-black absolute right-24 bottom-2 rounded-full bg-emerald-700 flex justify-center" : "littleCircleRight border-2 border-black absolute right-24 bottom-2 rounded-full bg-emerald-700 flex justify-center"}>
-            <StandardButton text={'Giphy Machine'} handleClick={handleGiphy}/>
+            <StandardButton text={'Giphy Machine'} />
         </div>
     </div>
     )
