7a02c939b6ffe5dfe7447e0400349973d8083215
Cosmin <cosmin_7d@yahoo.com>
2023-10-18 15:20:19 +0100

    Password validation working, if the correct password has entered by the user , the post request message should be true, if the user has no password, a message should say the has no password

--- src/Components/PasswordEntry/index.jsx
+++ src/Components/PasswordEntry/index.jsx
@@ -1,18 +1,33 @@
 import PasswordInput from "../PasswordInput/index.jsx";
 import StandardButton from "../StandardButton/index.jsx";
+import {useEffect} from "react";
 
-const PasswordEntry = ({username}) => {
+
+const PasswordValidation = ({enteredPassword, userId}) => {
+
+    const checkPassword = async () => {
+         await fetch (`http://localhost:8080/checkPassword`,
+             {
+                 method: 'POST',
+                 body: JSON.stringify({user:userId, password:enteredPassword})
+             })
+    }
+
+    useEffect(() => {
+        checkPassword().catch('Unexpected error ocurred!')
+    }, []);
+
     return (
         <>
-            <div>{username}</div>
+            {/*<div>{username}</div>*/}
             <div>Enter Password</div>
             <PasswordInput/>
             <div>
-                <StandardButton text={"Submit"}/>
+                <StandardButton text={"Submit"} onClick={checkPassword}/>
                 <StandardButton text={"Go Back"}/>
             </div>
         </>
     )
 }
 
-export default PasswordEntry
+export default PasswordValidation
