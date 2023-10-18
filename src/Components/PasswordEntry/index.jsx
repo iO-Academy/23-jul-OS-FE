874b780c282c8cb02dfaade134c987bfeb4edc59
Cosmin <cosmin_7d@yahoo.com>
2023-10-18 16:31:11 +0100

    Added useState

--- src/Components/PasswordEntry/index.jsx
+++ src/Components/PasswordEntry/index.jsx
@@ -1,11 +1,11 @@
 import PasswordInput from "../PasswordInput/index.jsx";
 import StandardButton from "../StandardButton/index.jsx";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {redirect} from "react-router-dom";
 
 
-const PasswordValidation = ({enteredPassword, userId}) => {
-
+const PasswordEntry = ({enteredPassword, userId}) => {
+    const [passwordMatch, setPasswordMatch] = useState()
     const checkPassword = async () => {
          const response = await fetch (`http://localhost:8080/checkPassword`,
              {
@@ -13,16 +13,22 @@ const PasswordValidation = ({enteredPassword, userId}) => {
                  body: JSON.stringify({user:userId, password:enteredPassword})
              })
         const data = await response.json()
-        if (data.success) {
-            return redirect("/desktop")
+        if (data.passwordMatch) {
+            redirect('/desktop')
+        } else {
+            setPasswordMatch(false)
         }
-        return null;
     }
 
     return (
         <>
             {/*<div>{username}</div>*/}
             <div>Enter Password</div>
+            <div>
+                {
+                    !passwordMatch ? 'Password does not match db' : ''
+                }
+            </div>
             <PasswordInput/>
             <div>
                 <StandardButton text={"Submit"} onClick={checkPassword}/>
@@ -32,4 +38,4 @@ const PasswordValidation = ({enteredPassword, userId}) => {
     )
 }
 
-export default PasswordValidation
+export default PasswordEntry
