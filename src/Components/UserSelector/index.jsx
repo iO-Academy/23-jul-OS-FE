b379f8e128faf59800f3faf40bcff9be8d8af602
iO Academy <thejoshsilasbryan@gmail.com>
2023-10-18 16:04:56 +0100

    Users now display, will direct you to password screen if password needed or desktop if not

--- src/Components/UserSelector/index.jsx
+++ src/Components/UserSelector/index.jsx
@@ -1,56 +1,58 @@
 import './UserSelector.css'
 import StandardButton from "../StandardButton/index.jsx";
 import {useEffect, useState} from "react";
-import {Link} from "react-router-dom";
-import Desktop from "../Desktop/index.jsx";
+import {Link, useNavigate} from "react-router-dom";
 
-const UserSelector = ({username, icon, visible, passWordInput, openProgram, activeProgram}) => {
-    //
-    // const handleClick = () => {
-    //     console.log('button clicked')
-    // }
+
+const UserSelector = () => {
 
     const [users, setUsers] = useState(false)
     const [user, setUser] = useState()
+    const navigate = useNavigate();
 
     const getAllUsers = async () => {
         let response = await fetch ('http://0.0.0.0:8080/users/')
         let usersData = await response.json()
         setUsers(usersData.data)
+
     }
 
     useEffect(() => {
         getAllUsers()
     }, []);
 
-    const userLocations = [
-        {userName: username, location: 'animatedCircleLeft', horizontal: 'left', vertical: 'top'},
-        {location: 'animatedCircleLeft', horizontal: 'left', vertical: 'bottom'},
-        {location: 'animatedCircleRight', horizontal: 'right', vertical: 'top'},
-        {location: 'animatedCircleRight', horizontal: 'right', vertical: 'bottom'},
-    ]
+
     const createUserNameArray = () => {
         return users.map(user => {
             return <div className= "getUserButton border-2 border-black rounded-full bg-emerald-700 flex justify-center" key={user.id}>
         <StandardButton
             key={user.id}
             text={user.username}
-            onClick={passWordInput} />
+            handleClick={() => {attemptToLogin(user.needspassword)}} />
             </div>
             })
     }
 
+    const attemptToLogin = (needspassword) => {
+        console.log('dghdfg')
+        if (needspassword) {
+            return navigate("/passwordEntry")
+        } else {
+            return navigate ("/desktop")
+        }
+    }
+
     return (
         <>
-        <div className ='movingButtonsBottomToTop flex justify-center'>{users && createUserNameArray(userLocations)} </div>
+        <div className ='movingButtonsBottomToTop flex justify-center'>{users && createUserNameArray()} </div>
         <div className = "flex justify-center flex-wrap flex-col content-center">
-            <container className="WelcomeCircle rounded-full bg-green-900 bg-opacity-45 mt-20 z-10 flex justify-center flex-wrap content-center">
+            <div className="WelcomeCircle rounded-full bg-green-900 bg-opacity-45 mt-20 z-10 flex justify-center flex-wrap content-center">
                 <p className='mt-10'>Please select user</p>
                 <p>or log in as guest</p>
                 <div className="getProgramButton rounded-full bg-emerald-700 flex justify-center">
                     <Link className='flex justify-center' to={"/desktop"}><StandardButton text ={'Guest'}/></Link>
                 </div>
-            </container>
+            </div>
             </div>
 </>
     )
