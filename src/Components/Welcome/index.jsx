9508883c145b715d7a96f2b5b5fcd4cedb3870c1
iO Academy <thejoshsilasbryan@gmail.com>
2023-10-19 11:41:46 +0100

    Yes too scared to touch W

--- src/Components/Welcome/index.jsx
+++ src/Components/Welcome/index.jsx
@@ -2,7 +2,6 @@
 import StandardButton from "../StandardButton/index.jsx";
 import {Link} from "react-router-dom";
 
-
 const Welcome = () => {
     const handleClick = () => {
     }
@@ -12,11 +11,11 @@ const Welcome = () => {
             <div className = "flex justify-center flex-wrap flex-col content-center mt-10">
                 <p>Welcome to</p>
                 <p>CaterPillar OS</p>
-                <container className="WelcomeCircle mt-10 rounded-full bg-green-900 bg-opacity-45 mt-0 z-10 flex justify-center flex-wrap flex-col content-center">
+                <div className="WelcomeCircle mt-10 rounded-full bg-green-900 bg-opacity-45 z-10 flex justify-center flex-wrap flex-col content-center">
                     <div className="getProgramButton rounded-full bg-emerald-700 mt-20 flex justify-center">
                         <Link className='flex justify-center' to={"/userSelector"}><StandardButton text ={'Enter'} onClick={handleClick} /></Link>
                     </div>
-                </container>
+                </div>
             </div>
         </>
     )
