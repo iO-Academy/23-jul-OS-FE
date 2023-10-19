f42f0d7677c46b2cc33fb33fb2df69e37794fb56
iO Academy <thejoshsilasbryan@gmail.com>
2023-10-19 14:00:20 +0100

    Navbar added

--- src/Components/Clock/index.jsx
+++ src/Components/Clock/index.jsx
@@ -1,26 +1,27 @@
 import {useState, useEffect} from "react";
 
 const Clock = () => {
-
     const [time, setTime] = useState('')
 
-    useEffect(() => {
+    const clockFunction = () => {
+        const dateObject = new Date()
+        const month = dateObject.toLocaleString('en-US', {month: 'short'})
+        const day = dateObject.getDate()
+        const hour = dateObject.getHours()
+        let minute = dateObject.getMinutes()
+        if (minute < 10) {
+            minute = '0' + minute
+        }
+        const curTime = day + ' ' + month + ' ' + hour + ':' + minute
+        setTime(curTime)
+    }
 
-        setInterval(() => {
-            const dateObject = new Date()
-
-            const month = dateObject.toLocaleString('en-US', {month: 'short'})
-            const day = dateObject.getDate()
-            const hour = dateObject.getHours()
-            let minute = dateObject.getMinutes()
-            if (minute < 10) {
-                minute = '0' + minute
-            }
-            const curTime = day + ' ' + month + ' ' + hour + ':' + minute
-            setTime(curTime)
-        }, 1000)
-    }, [])
-    return <div>{time}</div>
+        useEffect(() => {
+            clockFunction()
+            setInterval(clockFunction, 1000)
+        }, [])
+        return <div>{time}</div>
 }
+
 
 export default Clock
--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -6,9 +6,11 @@ import {Link} from "react-router-dom";
 const NavBar = ({openMenu}) => {
 
     return (
-        <div className=" fixed w-full bg-purple-100 flex justify-between px-5 py-2 text-xl">
+        <div className="fixed w-full bg-purple-100 flex justify-between px-5 py-2 text-xl">
             <Link to={"/"}><NavButton text="Logout" /> </Link>
+                <div className='flex ml-10'>
             <NavButton text="Menu" handleClick={openMenu} />
+        </div>
             <Clock />
         </div>
     )
