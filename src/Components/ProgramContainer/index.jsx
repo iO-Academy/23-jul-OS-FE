895e3214924c8cdcfaf8c8d12abb830f425d88da
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-10-19 09:28:50 +0100

    PR Changes - header and close button tidied

--- src/Components/ProgramContainer/index.jsx
+++ src/Components/ProgramContainer/index.jsx
@@ -1,15 +1,16 @@
 import './ProgramContainer.css'
 import Draggable from "react-draggable";
 import AboutPage from "../AboutPage/index.jsx";
+import {GrClose} from "react-icons/gr";
 
 const ProgramContainer = ({toggleProgram, text, children}) => {
     return (
         <Draggable handle="header">
             <div className="program resize overflow-auto">
-                <header className="header h-16 bg-green-700 text-center">
+                <header className="header h-16 bg-green-700 pt-4 text-center">
                     <span className="text-3xl">{text}</span>
-                    <span className="close-btn absolute top-2 right-2" onClick={() => toggleProgram(false)}>Close</span>
                 </header>
+                <span className="close-btn absolute top-2 right-2" onClick={() => toggleProgram(false)}><GrClose /></span>
                 <div className="program-content my-10 mx-20 p-5 h-auto">
                     {children}
                 </div>
